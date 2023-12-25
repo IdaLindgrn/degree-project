55d67117b99106315307ae9091f57aa7931c7ba7
Ida <ida.nacka@gmail.com>
2023-12-25 16:07:22 +0100

    updated .env names

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import StartPage from './views/GameWebsite/StartPage.vue';
 </script>
 
--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl: string = import.meta.env.REACT_APP_SUPABASE_URL!;
-const supabaseKey: string = import.meta.env.REACT_APP_ANON_KEY!;
+const supabaseUrl: string = import.meta.env.VITE_REACT_APP_SUPABASE_URL!;
+const supabaseKey: string = import.meta.env.VITE_REACT_APP_ANON_KEY!;
 
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
