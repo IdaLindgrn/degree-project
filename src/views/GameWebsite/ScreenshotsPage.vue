3298607b2e1f19b8d59a18742d2439da83d56676
Ida <ida.nacka@gmail.com>
2024-01-14 23:48:44 +0100

    updated screenshots screen

--- src/views/GameWebsite/ScreenshotsPage.vue
+++ src/views/GameWebsite/ScreenshotsPage.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
       <Header/>
-      <h1>Screenshots Page</h1>
-      <font-awesome-icon icon="fa-solid fa-user-secret" />
+      <div class="content-container">
+        <h1 class="text-screenshots">Screenshots</h1>
+        <div class="image-container">
+          <img class="image" src="../../assets/screenshots.png" alt="Screenshot">
+        </div>
+      </div>
     </div>
   </template>
   
+  <style scoped lang="scss">
+  @import '@/style/main';
+  .content-container {
+    padding: 30px;
+    text-align: center;
+    color: $primaryFontColor;
+  }
+
+  .text-screenshots {
+    padding-bottom: 20px;
+  }
+  
+  .image {
+    max-width: 70vw; 
+    margin: 0 auto;
+  }
+  </style>
+  
   <script setup lang="ts">
   import Header from '../../components/Header.vue';
-  
-  
   </script>
-  
   
