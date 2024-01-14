349c75f90387c4721614485bdec1c8e8941d865e
Ida <ida.nacka@gmail.com>
2024-01-14 23:54:40 +0100

    updated text

--- src/views/GameWebsite/TutorialPage.vue
+++ src/views/GameWebsite/TutorialPage.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>
+  <div class="tutorial-container">
     <Header/>
-    <h1>About Page</h1>
-    <font-awesome-icon icon="fa-solid fa-user-secret" />
+    <div class="tutorial-text">
+    <h1>Tutorial</h1>
+    <div class="instructions">
+      <p>Move the cats to their boxes using Flexbox!</p>
+    </div>
+   </div>
   </div>
 </template>
 
+<style scoped lang="scss">
+  @import '@/style/main';
+.tutorial-container {
+  text-align: center;
+}
+
+.tutorial-text {
+  padding: 30px;
+  color: $primaryFontColor;
+}
+
+.instructions {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+</style>
+
 <script setup lang="ts">
 import Header from '../../components/Header.vue';
-
-
 </script>
-
-
-
